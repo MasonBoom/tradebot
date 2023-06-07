d8e2317e6e6c1c51256eec3e00a6d85bce394cfe
MasonBoom <masonboom@gmail.com>
2023-06-06 22:05:53 -0400

    Installed tensorflow and starteed model

---
+++ src/models/predictionModel.js
@@ -0,0 +1,33 @@
+import * as tf from '@tensorflow/tfjs';
+
+// Define your machine learning model
+const createModel = (inputShape) => {
+  const model = tf.sequential();
+  model.add(tf.layers.lstm({ units: 50, inputShape }));
+  model.add(tf.layers.dense({ units: 1 }));
+  return model;
+};
+
+// Train the model
+const trainModel = async (model, XTrain, yTrain, epochs, batchSize) => {
+  await model.fit(XTrain, yTrain, { epochs, batchSize, shuffle: true });
+};
+
+// Evaluate the model
+const evaluateModel = (model, XTest, yTest) => {
+  const evaluation = model.evaluate(XTest, yTest);
+  return evaluation;
+};
+
+// Make predictions
+const makePredictions = (model, XUnseen) => {
+  const predictions = model.predict(XUnseen);
+  return predictions;
+};
+
+export {
+  createModel,
+  trainModel,
+  evaluateModel,
+  makePredictions,
+};
